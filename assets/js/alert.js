733bf14ebddffc63961895164080c005471bd57f
RobbyCaturW <robbycatur330@gmail.com>
2024-12-12 08:30:07 +0700

    ProjectK3

---
+++ assets/js/alert.js
@@ -0,0 +1,19 @@
+const navItems = document.querySelectorAll('nav ul li a');
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        alert(`Anda akan beralih ke halaman ${item.textContent}`);
+    });
+});
+
+const button = document.querySelectorAll('section ol ul li');
+button.forEach(item => {
+    item.addEventListener('click', () => {
+        alert(`Anda akan membaca dokumen ${item.textContent}`);
+    });
+});
+const button2 = document.querySelectorAll('section ul li');
+button2.forEach(item => {
+    item.addEventListener('click', () => {
+        alert(`Anda akan membaca dokumen ${item.textContent}`);
+    });
+});
